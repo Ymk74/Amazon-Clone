b9e79034f8617a32a6d4ec889c077feeb6370a6d
ymk74 <swimmer.sahrk12@gmail.com>
2023-08-29 15:35:29 +0300

    add admin cutomization for product and admin-Tabular

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Product , ProductImages ,Brand ,Review
 
 # Register your models here.
+
+class ProductImagesTabular(admin.TabularInline):
+    model = ProductImages
+
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','flag','price','quantity','brand']
+    list_filter = ['flag','brand']
+    search_fields = ['name','subtitle','description']
+    inlines = [ProductImagesTabular]
+
+admin.site.register(Product,ProductAdmin)
+admin.site.register(ProductImages)
+admin.site.register(Brand)
+admin.site.register(Review)
--- product/models.py
+++ product/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth import User 
+from django.contrib.auth.models import User 
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 
