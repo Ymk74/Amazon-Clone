a14ddc03e401764562956f8c97a7570bca3d1713
ymk74 <swimmer.sahrk12@gmail.com>
2023-09-01 04:15:26 +0300

    add product slug

--- product/models.py
+++ product/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from taggit.managers import TaggableManager
+from django.utils.text import slugify
 
 # Create your models here.
 FLAG_TYPES =[
@@ -22,10 +23,15 @@ class Product(models.Model):
     quantity = models.IntegerField(_('Quantity'))
     brand = models.ForeignKey('Brand',verbose_name=_('Brand'), related_name='produc_brand', on_delete=models.SET_NULL, null=True)
     tags = TaggableManager()
+    slug = models.SlugField(null=True , blank=True)
 
     def __str__(self):
         return self.name
     
+
+    def save(self, *args, **kwargs):
+       self.slug = slugify(self.name) 
+       super(Product, self).save(*args, **kwargs) # Call the real save() method
 
 
 class ProductImages(models.Model):
