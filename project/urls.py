c3fd38e5e53d2972e80f22b0b60d0a201cfed7ee
ymk74 <swimmer.sahrk12@gmail.com>
2023-09-10 13:33:37 +0300

    add home

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('product.urls')),
+    path('', include('settings.urls')),
     path("__debug__/", include("debug_toolbar.urls")),
 ]
 
--- settings/views.py
+++ settings/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+from django.db.models import Count
+from product.models import Product , Brand , Review
 # Create your views here.
+
+def home(request):
+    brands = Brand.objects.all().annotate(product_count=Count('product_brand'))
+    sale_products = Product.objects.filter(flag='Sale')[:10]
+    feature_products = Product.objects.filter(flag='Feature')[:6]
+    new_products = Product.objects.filter(flag='New')[:10]
+    reviews = Review.objects.all()[:5]
+    return render(request,'settings/home.html',{
+        'brands':brands ,
+        'sale_products':sale_products ,
+        'feature_products':feature_products ,
+        'new_products':new_products ,
+        'reviews':reviews ,
+    })
