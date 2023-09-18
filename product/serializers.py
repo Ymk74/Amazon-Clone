ac90c93943cc5bc4f27a0f4bd2a2f778a3d65240
ymk74 <swimmer.sahrk12@gmail.com>
2023-09-18 14:58:31 +0300

    add brand in product list , add  reviews_count , avg_rate product detail

--- product/serializers.py
+++ product/serializers.py
@@ -3,14 +3,24 @@ from .models import Product ,Brand
 from django.db.models.aggregates import Avg
 
 
+class BrandListSerializer(serializers.ModelSerializer):
+    class Meta :
+        model = Brand
+        fields = '__all__'
+
+
 class ProductListSerializer(serializers.ModelSerializer):
+#  brand = BrandListSerializer()
+    brand = serializers.StringRelatedField()
     avg_rate = serializers.SerializerMethodField()
     reviews_count = serializers.SerializerMethodField()
-    price_with_tax = serializers.SerializerMethodField()
+#    price_with_tax = serializers.SerializerMethodField()
     class Meta :
         model = Product
         fields = '__all__'
 
+# def get_price_with_tax(self,product):
+    #    return product.price*1.5
     
     def get_avg_rate(self,product):
         avg = product.review_product.aggregate(rate_avg=Avg('rate'))
@@ -24,23 +34,26 @@ class ProductListSerializer(serializers.ModelSerializer):
         return reviews 
 
 
-    def get_price_with_tax(self,product):
-        return product.price*1.5
-
-
-
-
 class ProductDetailSerializer(serializers.ModelSerializer):
+    avg_rate = serializers.SerializerMethodField()
+    reviews_count = serializers.SerializerMethodField()
     class Meta :
         model = Product
         fields = '__all__'
 
 
+    def get_avg_rate(self,product):
+        avg = product.review_product.aggregate(rate_avg=Avg('rate'))
+        if not avg['rate_avg'] :
+            result = 0
+            return result
+        return avg['rate_avg']
 
-class BrandListSerializer(serializers.ModelSerializer):
-    class Meta :
-        model = Brand
-        fields = '__all__'
+    def get_reviews_count(self,product:Product):
+        reviews = product.review_product.all().count()
+        return reviews 
+
+
 
 
 class BrandDetailSerializer(serializers.ModelSerializer):
