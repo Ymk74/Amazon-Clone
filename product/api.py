f76ffc82b455a9e112f43d36d0ca7a258ed87b9d
ymk74 <swimmer.sahrk12@gmail.com>
2023-09-18 23:43:26 +0300

    add custom filter

--- product/api.py
+++ product/api.py
@@ -1,6 +1,7 @@
 from .serializers import ProductListSerializer , ProductDetailSerializer ,BrandListSerializer , BrandDetailSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from .myfilter import ProductFilter
 from .models import Product , Brand
 from rest_framework import generics
 
@@ -24,11 +25,10 @@ class ProductListApi(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductListSerializer
     filter_backends = [DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
-    filterset_fields = ['flag', 'brand']
+    filterset_fields = ['flag', 'brand' , 'price']
     search_fields = ['name', 'subtitle' , 'description']
     ordering_fields = ['price', 'quantity']
-
-
+    filterset_class = ProductFilter
 
 
 
