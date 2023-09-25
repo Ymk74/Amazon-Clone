51144fcb418fc5c21cd092715fbc007c7c7899fd
ymk74 <swimmer.sahrk12@gmail.com>
2023-09-25 19:53:30 +0300

    design order , cart , coupon

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
+import datetime
+from django.utils import timezone
+from utils.generate_code import generate_code
 
-# Create your models here.
+CART_STATUS = (
+    ('InProgress' , 'InProgress'),
+    ('Completed' , 'Completed'),
+)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User,related_name='cart_user',on_delete=models.SET_NULL,null=True,blank=True)
+    status = models.CharField(max_length=10,choices=CART_STATUS)
+
+    def __str__(self):
+        return self(self.user)
+    
+
+
+class CartDetail(models.Model):
+    cart = models.ForeignKey(Cart,related_name='cart_detail',on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product,related_name='cart_product',on_delete=models.SET_NULL,null=True,blank=True)
+    quantity = models.IntegerField()
+    total = models.FloatField(null=True,blank=True)
+
+    def __str__(self):
+        return self(self.cart)
+    
+
+
+
+ORDER_STATUS = (
+    ('Received','Received'),
+    ('Processed','Processed'),
+    ('Shipped','Shipped'),
+    ('Delivered','Delivered')
+)
+
+class Order(models.Model):
+    user = models.ForeignKey(User,related_name='order_user',on_delete=models.SET_NULL,null=True,blank=True)
+    status = models.CharField(max_length=10,choices=ORDER_STATUS)
+    code = models.CharField(max_length=20,default=generate_code())
+    order_time = models.DateTimeField(default=timezone.now)
+    delivery_time = models.DateTimeField(null=True,blank=True)
+    coupon = models.ForeignKey('Coupon',related_name='order_coupon',on_delete=models.SET_NULL,null=True,blank=True)
+    total_After_coupon = models.FloatField(null=True,blank=True)
+    def __str__(self):
+        return self(self.user)
+    
+
+
+
+
+class OrderDetail(models.Model):
+    order = models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
+    Product = models.ForeignKey(Product,related_name='order_product',on_delete=models.SET_NULL,null=True,blank=True)
+    price = models.FloatField()
+    quantity = models.IntegerField()
+    total = models.FloatField(null=True,blank=True)
+    # coupon = models.ForeignKey()
+    def __str__(self):
+        return self(self.order)
+    
+
+
+
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=20)
+    discount = models.IntegerField()
+    quantity = models.IntegerField()
+    start_date = models.DateField(default=timezone.now)
+    end_date = models.DateField(null=True,blank=True)
+
+    def __str__(self):
+        return self.code
+    
+    def save(self, *args, **kwargs):
+        week = datetime.timedelta(days=7)
+        self.end_date = self.start_date + week
+        super(ModelName, self).save(*args, **kwargs) # Call the real save() method
