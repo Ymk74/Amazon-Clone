053618340ccc5e5fcf9443e91b04fa4165370e87
ymk74 <swimmer.sahrk12@gmail.com>
2023-09-26 19:28:58 +0300

    add LoginRequired for orders , checkout

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Order ,Cart , CartDetail,OrderDetail , Coupon
 
 
-class OrderList(ListView):
+class OrderList(LoginRequiredMixin,ListView):
     model = Order
     paginate_by = 10
 
@@ -14,6 +16,9 @@ class OrderList(ListView):
 
 
 
+
+
+@login_required
 def checkout(request):
     cart = Cart.objects.get(user=request.user,status='InProgress')
     cart_detail = CartDetail.objects.filter(cart=cart)
