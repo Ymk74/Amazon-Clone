a0add0b0091a2f72fe4d77ae064dd23764c6f273
ymk74 <swimmer.shark12@gmail.com>
2023-10-07 20:10:42 +0300

    add delete cart detail api

--- orders/api.py
+++ orders/api.py
@@ -24,4 +24,9 @@ class CartDetailCreateAPI(generics.GenericAPIView):
 
 
     def delete(self,request,*args, **kwargs):
-        pass
+        user = User.objects.get(username=self.kwargs['username'])
+        cart_detail = CartDetail.objects.get(id=request.data['cart_detail_id'])
+        cart_detail.delete()
+        cart = Cart.objects.get(user=user,status='InProgress')
+        data = CartSerializer(cart).data
+        return Response({'message':'product deleted successfully', 'cart':data})
