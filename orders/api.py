203dd7518be55c6119cb9ac99e26a7bd969e77d3
ymk74 <swimmer.shark12@gmail.com>
2023-10-07 21:07:50 +0300

    add order list api

--- orders/api.py
+++ orders/api.py
@@ -2,8 +2,8 @@ from rest_framework import generics
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User 
-from .serializers import CartSerializer 
-from .models import Cart , CartDetail
+from .serializers import CartSerializer , OrderDetailSerializer , OrderListSerializer
+from .models import Cart , CartDetail , Order , OrderDetail
 from product.models import Product
 
 
@@ -28,7 +28,7 @@ class CartDetailCreateAPI(generics.GenericAPIView):
         cart_detail.quantity = quantity
         cart_detail.total = round( quantity* product.price, 2)
         cart_detail.save()
-        
+
         cart = Cart.objects.get(user=user,status='InProgress')
         data = CartSerializer(cart).data
         return Response({'message':'product added successfully', 'cart':data})
@@ -44,3 +44,24 @@ class CartDetailCreateAPI(generics.GenericAPIView):
         cart = Cart.objects.get(user=user,status='InProgress')
         data = CartSerializer(cart).data
         return Response({'message':'product deleted successfully', 'cart':data})
+
+
+
+class OrderListAPI(generics.ListAPIView):
+    serializer_class = OrderListSerializer
+    queryset = Order.objects.all()
+
+
+    def list(self,request,*args, **kwargs):
+        user = User.objects.get(username=self.kwargs['username'])
+        queryset = self.get_queryset().filter(user=user)
+        data = OrderListSerializer(queryset,many=True).data
+        return Response(data)
+
+
+
+    # def get_queryset(self):
+    #     user = User.objects.get(username=self.kwargs['username'])
+    #     queryset = super(OrderListAPI, self).get_queryset()
+    #     queryset = queryset.filter(user=user)
+    #     return queryset
--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import OrderList , checkout ,add_to_cart , remove_from_cart
-from .api import CartDetailCreateAPI
+from .api import CartDetailCreateAPI , OrderListAPI
 
 
 
@@ -14,5 +14,6 @@ urlpatterns = [
 
 
     # api
-    path('api/<str:username>/cart', CartDetailCreateAPI.as_view()), 
+    path('api/<str:username>/cart', CartDetailCreateAPI.as_view()),
+    path('api/list/<str:username>',OrderListAPI.as_view()),
 ]
