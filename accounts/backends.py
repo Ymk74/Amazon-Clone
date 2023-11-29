1abb8f60559f69d27853f02a008eb052a61fbb00
ymk74 <swimmer.shark12@gmail.com>
2023-11-29 15:05:09 +0200

    add email backend

---
+++ accounts/backends.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth.models import User
+
+class EmailOrUsernameModelBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = User.objects.get(email=username)
+        except User.DoesNotExist:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return None
+
+        if user.check_password(password):
+            return user
+        return None
--- project/settings.py
+++ project/settings.py
@@ -213,3 +213,8 @@ LANGUAGES = [
 # settings.py
 CELERY_BROKER_URL = 'redis://myredis:6379/0'
 CELERY_RESULT_BACKEND = 'redis://myredis:6379/0'
+
+AUTHENTICATION_BACKENDS = (
+    'accounts.backends.EmailOrUsernameModelBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
