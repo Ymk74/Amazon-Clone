8f0c1b7aec2636376fcf79db947a904e61bc8bda
ymk74 <swimmer.shark12@gmail.com>
2023-11-30 13:30:32 +0200

    add accounts view structure

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def signup(request):
+    pass
+
+
+
+def activate(request):
+    pass
+
+
+
+def profile(request):
+    pass
--- project/urls.py
+++ project/urls.py
@@ -47,6 +47,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls",namespace='accounts')),
     path('admin/', admin.site.urls),
     path('products/', include('product.urls',namespace='products')),
     path('orders/', include('orders.urls',namespace='orders')),
