6d0553cb2b7b4315bdae584411fb78c08ce3f42a
ymk74 <swimmer.shark12@gmail.com>
2023-12-04 14:46:10 +0200

    add signup , activate

--- accounts/views.py
+++ accounts/views.py
@@ -12,8 +12,11 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
-            email = form.changed_data['password']
-            form.save()
+            email = form.cleaned_data['email']
+            
+            user = form.save(commit=False)
+            user.is_activate = False
+            user.save()
             
             profile = Profile.objects.get(user__username=username)
             
@@ -25,17 +28,35 @@ def signup(request):
                 fail_silently=False,
                 )
                 
-            return redirect('/accounts/{username}/activate')
+            return redirect(f'/accounts/{username}/activate')
             
     else :
         form = SignupForm()
-
     return render(request, 'registration/register.html',{'form':form})
 
 
 
 def activate(request,username):
-    pass
+    profile = Profile.objects.get(user__username=username)
+    if request.method == 'POST':
+        form = ActivationForm(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            if code == profile.code :
+                profile.code = ''
+                profile.save()
+
+                user = User.objects.get(user=profile.user)
+                user.is_activate = True
+                user.save()
+                
+
+
+                return redirect('/accounts/login')
+    else :
+        form = ActivationForm()
+
+    return render(request,'registration/activate.html',{'form':form})
 
 
 
